61be2fc689eab33a28505469732ce3902b38e9c8
MAXX <fernandesjaymark@yahoo.com>
2021-10-19 22:48:54 +0800

    table super admin with dashboard:

---
+++ resources/views/layout/breadcrumbs-and-year.blade.php
@@ -0,0 +1,7 @@
+<div class="d-flex align-items-center justify-between p-4">
+    <h2 class="text-xl font-bold">Menu Dummy Title of BreadCrumbs</h2>
+    <a href="#">link or modal to change year</a>
+</div>
+<div class="d-block px-4 ">
+    <span class="text-md font-bold text-purple-600"><i class="fas fa-calendar-alt mr-2"></i>January 01 1990</span>
+</div>  
