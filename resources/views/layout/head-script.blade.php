41ac190d89c15442aa8546399f73ff43a5ed13a8
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-15 20:45:07 +0800

    Added pusher notif

--- resources/views/layout/head-script.blade.php
+++ resources/views/layout/head-script.blade.php
@@ -8,6 +8,9 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
         <link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css">
         <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+
+        {{-- Will change this when login is ready --}}
+        {{-- @if (auth()->check()) --}}
         <script>
             // Enable pusher logging - don't include this in production
             Pusher.logToConsole = true;
@@ -16,14 +19,13 @@
                 cluster: '{{ env("PUSHER_APP_CLUSTER") }}',
             });
 
-            var channel = pusher.subscribe('channel-name');
-            channel.bind('App\\Events\\PPMPNotif', function(data) {
+            var channel = pusher.subscribe('new-item-detail');
+            channel.bind('App\\Events\\NewItemDetail', function(data) {
                 console.log(data)
                 alert(JSON.stringify(data))
-                // app.messages.push(JSON.stringify(data)); //Data assignment
             });
         </script>
-
+        {{-- @endif --}}
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
@@ -31,7 +33,7 @@
                 padding:0;
             }
             div#delete--modal {
-                      background: rgba(0,0,0,.4);
+                    background: rgba(0,0,0,.4);
             }
             .ItemCateg__editModal {
                 background: rgba(0,0,0,.4);
