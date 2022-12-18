078244046ace72405b24e1e0240385709c4479b4
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-18 13:54:09 +0800

    replaced jquery with datatable

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -10,4 +10,4 @@
       </div>
    </div>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
+<script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js" defer></script>
