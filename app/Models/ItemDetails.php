747612263cba8783790ddfee6fb48d669e3bf32a
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-18 13:51:18 +0800

    Added the added_by field

--- app/Models/ItemDetails.php
+++ app/Models/ItemDetails.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\ItemCategories;
 use App\Models\Unit;
+use App\Models\User;
 
 class ItemDetails extends Model
 {
     use HasFactory;
-    protected $fillable = ['category_id', 'description', 'article', 'unit_id', 'unit_name', 'price_catalogue', 'is_approved', 'approved_by'];
+    protected $fillable = ['category_id', 'description', 'article', 'unit_id', 'unit_name', 'added_by', 'price_catalogue', 'is_approved', 'approved_by'];
 
     public function category()
     {
