808a5227e68b1603938d706ea6ce1665eb100131
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-19 17:08:57 +0800

    Added CSRF token to metadata

--- resources/views/layout/head-script.blade.php
+++ resources/views/layout/head-script.blade.php
@@ -1,6 +1,7 @@
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf" value="{{ csrf_token() }}">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>BULSU e-PROCUREMENT v1.1</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
